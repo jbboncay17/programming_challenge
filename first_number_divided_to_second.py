34ce7a9bbb055903d0ae4d4cc369365feb400fa7
jbboncay17 <jbboncay1@gmail.com>
2026-05-01 19:19:06 +0800

    added if and else condition

--- first_number_divided_to_second.py
+++ first_number_divided_to_second.py
@@ -1,2 +1,10 @@
-firstnumber=(input("Enter the first number: "))
-secondnumber=(input("Enter the second number: "))
+firstnumber=int(input("Enter the first number: "))
+secondnumber=int(input("Enter the second number: "))
+
+if secondnumber !=0:
+    remainder=firstnumber%secondnumber
+    print("")
+    print("The remainder is:", remainder)
+
+else:
+    print("Syntax Error")
